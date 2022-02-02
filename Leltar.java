df5f39597818db3f52ae40ed547e0602b35157e0
Aszellem <74776542+Aszellem@users.noreply.github.com>
2022-02-02 19:53:38 +0100

    setTitle

--- Leltar.java
+++ Leltar.java
@@ -22,7 +22,7 @@ public class Leltar extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.setTitle("Leltár");
         stage.setScene(scene);
         stage.show();
     }
